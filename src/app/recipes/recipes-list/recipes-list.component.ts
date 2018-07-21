cad8f80ebd26b967b7b936d6c81af3929b42bfb6
abrl91 <amit.barletz@outlook.com>
2018-07-21 12:46:56 +0300

    #

--- src/app/recipes/recipes-list/recipes-list.component.ts
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 import { Recipe } from '../recipe.model';
+import { RecipeService } from '../recipe.service';
 //import { EventEmitter } from 'events';
 
 @Component({
@@ -9,19 +10,22 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipes-list.component.css']
 })
 export class RecipesListComponent implements OnInit {
-  @Output() recipeWasSelected = new EventEmitter<Recipe>();
-  recipes: Recipe[] = [
-    new Recipe('A test recipe-1', 'This is a test', 'https://latelier-du-burger-nice.fr/wp-content/uploads/2017/11/veggie-burger-0.jpg'),
-    new Recipe('A test recipe-2', 'This is another test', 'https://cdn.jamieoliver.com/recipe-database/xtra_med/51137672.jpg')
-  ];
+  // @Output() recipeWasSelected = new EventEmitter<Recipe>();
+  
+  recipes: Recipe[];
+  // recipes: Recipe[] = [
+  //   new Recipe('A test recipe-1', 'This is a test', 'https://latelier-du-burger-nice.fr/wp-content/uploads/2017/11/veggie-burger-0.jpg'),
+  //   new Recipe('A test recipe-2', 'This is another test', 'https://cdn.jamieoliver.com/recipe-database/xtra_med/51137672.jpg')
+  // ];
 
-  constructor() { }
+  constructor(private recipeService: RecipeService) { }
 
   ngOnInit() {
+    this.recipes = this.recipeService.getRecipes();
   }
 
-  onRecipeSelected(recipe: Recipe) {
-    this.recipeWasSelected.emit(recipe);
-  }
+  // onRecipeSelected(recipe: Recipe) {
+  //   this.recipeWasSelected.emit(recipe);
+  // }
 
 }
